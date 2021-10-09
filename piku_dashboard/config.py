4d4b976a9a92d310ff9a880eab6e59364a6680fe
Rowan Twell <rowantwell@gmail.com>
2021-10-09 20:57:40 +0100

    use correct conf name

--- piku_dashboard/config.py
+++ piku_dashboard/config.py
@@ -9,4 +9,4 @@ USERNAME = os.environ.get("USERNAME", None)
 PASSWORD = os.environ.get("PASSWORD", None)
 
 # The dashboard's own application name in piku. this setting disallows shutdowns and destroy requests to the self app
-SELF_NAME = os.environ.get("SELF_APP_NAME", "piku_dashboard")
+SELF_APP_NAME = os.environ.get("SELF_APP_NAME", "piku_dashboard")
