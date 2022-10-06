2cce546a43dcd885edae2ba6288958540a0a69f5
Rowan <rowantwell@gmail.com>
2022-10-06 02:09:58 +0100

    minor fixes

--- piku_dashboard/config.py
+++ piku_dashboard/config.py
@@ -7,6 +7,3 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 USERNAME = os.environ.get("USERNAME", None)
 PASSWORD = os.environ.get("PASSWORD", None)
-
-# The dashboard's own application name in piku. this setting disallows shutdowns and destroy requests to the self app
-SELF_APP_NAME = os.environ.get("SELF_APP_NAME", "piku_dashboard")
